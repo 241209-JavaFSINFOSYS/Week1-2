3df1c8f53ecb026dff652068c9206a2864755cc9
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-12-16 11:01:07 -0500

    character component done

--- React/hello-events-routing/src/Components/CharacterComponent/Character.tsx
+++ React/hello-events-routing/src/Components/CharacterComponent/Character.tsx
@@ -1,11 +1,21 @@
 //This will get rendered once for every Character in the Array in CharacterContainer
+
+import { CharacterInterface } from "../../Interfaces/CharacterInterface"
+
 //This will take props of the Character Interface to render Character data
-export const Character:React.FC<any> = (character:any) => {
+export const Character:React.FC<CharacterInterface> = (character:CharacterInterface) => {
 
 
+    //This component will render whatever character was passed in as props
     return(
         <>
-            
+            <div>
+                <h3>{character.name} belongs to house {character.house}</h3>
+                <h4>Wielding {character.weapon}</h4>
+            </div>
+            <div>
+                <h4>"{character.quote}"</h4>
+            </div>
         </>
     )
 
